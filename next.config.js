5b052a455ede71e12e72b55dd63f4751694c4b26
SHALINI JHA <shalinijha1008@gmail.com>
2025-09-19 22:43:40 +0530

    few changes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose']
   }
 };
 
